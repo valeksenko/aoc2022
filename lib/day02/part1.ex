73b588b94d331403cb6cf2b64c429bebc2d3f20c
Val Aleksenko <v.aleksenko@gmail.com>
2022-12-02 08:11:58 -0500

    d2p1

---
+++ lib/day02/part1.ex
@@ -0,0 +1,49 @@
+defmodule AoC2022.Day02.Part1 do
+  @moduledoc """
+    @see https://adventofcode.com/2022/day/2
+  """
+  @behaviour AoC2022.Day
+
+  @items %{
+    "A" => :rock,
+    "B" => :paper,
+    "C" => :scissors,
+    "X" => :rock,
+    "Y" => :paper,
+    "Z" => :scissors
+  }
+
+  @score %{
+    :rock => 1,
+    :paper => 2,
+    :scissors => 3
+  }
+
+  @impl AoC2022.Day
+  def run(data) do
+    data
+    |> Enum.map(&parse/1)
+    |> Enum.map(&play/1)
+    |> Enum.sum
+  end
+
+  defp parse(input) do
+    input
+    |> String.split
+    |> Enum.map(fn v -> @items[v] end)
+  end
+
+  defp play([player1, player2]) do
+    @score[player2] + winner(player1, player2)
+  end
+
+  defp winner(:rock, :scissors), do: 0
+  defp winner(:scissors, :paper), do: 0
+  defp winner(:paper, :rock), do: 0
+
+  defp winner(:scissors, :rock), do: 6
+  defp winner(:paper, :scissors), do: 6
+  defp winner(:rock, :paper), do: 6
+
+  defp winner(_, _), do: 3
+end
